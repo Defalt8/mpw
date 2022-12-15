80f3ceba6bdab178ea27414a82e4feff1f0e5999
Axel <the.defalt8@gmail.com>
2022-12-15 06:48:41 +0300

    update+fix: enums, window -- fix key event handling - remove shift modified keys event handling - set background color to window - add enable_vsync - remove key_[A-Z] enums

--- test/window.cpp
+++ test/window.cpp
@@ -18,6 +18,7 @@ int main()
 		window.on_mouse_move   = [&](int x, int y) { mw::log << "mouse move " << "  " << x << " " << y << mw::endl; };
 		window.on_mouse_wheel  = [&](int x, int y, int delta, bool vertical) { mw::log << "mouse wheel " << x << " " << y << " " << delta << " " << vertical << mw::endl; };
 		window.on_key = [&](mw::key key, bool down) { mw::log << "key " << cstr_key(key) << " " << down << mw::endl; };
+		window.enable_vsync();
 		assert(window.show());
 		mw::log << (char const *)glGetString(GL_VERSION) << mw::endl;
 		while(window)
@@ -25,8 +26,11 @@ int main()
 			mw::next_event(display, event, false) && mw::process_event(event);
 			if(window.make_current()) 
 			{
-				float v = fmodf(float(clock()) / float(CLOCKS_PER_SEC), 1.f);
-				glClearColor(v, .2f, .3f, 0.f);
+				float ct = float(clock()) / float(CLOCKS_PER_SEC);
+				float v1 = 1.f + cos(ct);         if(v1 > 1.f) v1 = 2.f - v1;
+				float v2 = 1.f + sin(1.38f * ct); if(v2 > 1.f) v2 = 2.f - v2;
+				float v3 = 1.f + cos(-0.9f * ct); if(v3 > 1.f) v3 = 2.f - v3;
+				glClearColor(v1, v2, v3, 1.f);
 				glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 				window.swap();
 			}
